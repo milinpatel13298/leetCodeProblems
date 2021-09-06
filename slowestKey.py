25b54ba4fbb83fe15000533894c2fe22ba86bcf2
Milin Patel <milinpatel13298@gmail.com>
2021-09-06 04:05:39 -0600

    added comments

--- slowestKey.py
+++ slowestKey.py
@@ -1,15 +1,18 @@
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-        slowestDuration, durationI=0,0
+        slowestDuration, durationI=0,0  #durationI will contain the duration of the key pressed for the ith iteration
         slowestKey=''
         for i in range(len(keysPressed)):
+            #calculate durationI
             if i==0: durationI = releaseTimes[i]
             else: durationI = releaseTimes[i] - releaseTimes[i-1]
+            #only proceed if durationI is longer (greater) than the current slowest duration
             if durationI>=slowestDuration:
                 if durationI==slowestDuration:
+                    #replace slowestKey if current key is lexicographically greater than the slowestKey
                     if slowestKey<keysPressed[i]:
                         slowestKey=keysPressed[i]
-                        slowestDuration=durationI
+                        slowestDuration=durationI   #update slowestDuration value if slowestKEy has been replaced
                 else:
                     slowestKey=keysPressed[i]
                     slowestDuration=durationI
